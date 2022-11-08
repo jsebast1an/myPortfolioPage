0d15228aa0a01081bbcedb2ad697d4c6b18b1e0a
jsebastianbox <juanse.lopez@bussbox.com>
2022-11-08 18:14:29 -0300

    avances en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import * as AiIcons from 'react-icons/ai'
 import './footer.css'
 import '../../App.css'
 
@@ -10,32 +11,47 @@ export default function footer() {
     bubble.style.left = `${x}px`;
     wrapper.appendChild(bubble);
     setTimeout(() => wrapper.removeChild(bubble), 1300);
-  };
+  }
 
   window.onmousemove = (e) => animateBubble(e.clientX)
 
 
   return (
     <div className='footer flexRowWrapContainer'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
 
-        <div className='footer__box'>
-          <img src='' alt='logo' ></img>
-          <p className='footer__p'>
-            
-          </p>
-        </div>
+      <div className='footer__box'>
+        <h5>Pages</h5>
+      </div>
 
-        <div className='footer__box'>
-          <h3>Pages</h3>
-        </div>
+      <div className='footer__box'>
+        <h5>Projects</h5>
+        <a target="_blank" rel='noreferrer' href="https://www.fb.com">
+            <AiIcons.AiOutlineGithub /> React Store
+        </a>
+        <a target="_blank" rel='noreferrer' href="https://www.fb.com">
+            <AiIcons.AiOutlineGithub /> E-commerce Backend
+        </a>
+        <a target="_blank" rel='noreferrer' href="https://www.fb.com">
+            <AiIcons.AiOutlineGithub /> Fake Chat
+        </a>
+      </div>
 
-        <div className='footer__box'>
-          <h3>Projects</h3>
-        </div>
-
-        <div className='footer__box'>
-          <h3>Social Networks</h3>
-        </div>
+      <div className='footer__box'>
+        <h5>Social Networks</h5>
+        <a target="_blank" rel='noreferrer' href="https://www.fb.com">
+            <AiIcons.AiFillFacebook /> Facebook
+        </a>
+        <a target="_blank" rel='noreferrer' href="https://www.fb.com">
+            <AiIcons.AiFillLinkedin /> LinkedIn
+        </a>
+        <a target="_blank" rel='noreferrer' href="https://www.fb.com">
+            <AiIcons.AiFillInstagram /> Instagram
+        </a>
+      </div>
 
       <div id="bubble-wrapper"></div>
     </div>  
