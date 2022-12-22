4cd88b0d1c9270dae2bd00c2bfc948b4e0cd6731
jsebastianbox <juanse.lopez@bussbox.com>
2022-12-22 16:15:41 -0500

    inicio border en box:description

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import Footer from '../components/Footer/Footer'
 
 /* css */
 import '../App.css'
+import '../css/homePage.css'
 
 
 
@@ -18,9 +19,20 @@ function HomePage() {
         <span></span>
         <span></span>
 
-        <h2>
-          HomePage
-        </h2>
+        <div className='flexRowWrapContainer'>
+          
+          <div className='flexCenterContainer description'>
+            <h2>Hi! I am Juan Sebastián</h2>
+            <div>
+              Me gusta esto y esto y esto y esto y esto y esto y esto y esto y esto y est o y esto
+            </div>
+          </div>
+
+          <div className='flexCenterContainer'>
+            
+          </div>
+
+        </div>
           
       </main>
       
