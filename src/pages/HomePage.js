a9f6a0adab63e841e35ee910b0122cf3d8df802f
jsebastianbox <juanse.lopez@bussbox.com>
2022-12-23 11:39:08 -0500

    description borders animation completed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -22,9 +22,19 @@ function HomePage() {
         <div className='flexRowWrapContainer'>
           
           <div className='flexCenterContainer description'>
-            <h2>Hi! I am Juan Sebastián</h2>
-            <div>
-              Me gusta esto y esto y esto y esto y esto y esto y esto y esto y esto y est o y esto
+            <tag className='span'></tag>
+            <div className='text'>
+            I am a developer who likes to create,
+            learn and who in the future wants to
+            start his own start-up.
+            I emphasize being hardworking, gentle
+            and creative to give quick and efficient
+            solutions. I have always had good
+            interpersonal relationships, I like to get
+            together with people who want to take
+            on the world. I am looking for a
+            challenging position where I can
+            continue to learn.
             </div>
           </div>
 
